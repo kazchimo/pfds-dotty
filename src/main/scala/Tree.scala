01f610d6ea65c4e5a1085af914696144c2aee14e
kazuki <pharcydetip@gmail.com>
2021-02-05 01:04:33 +0900

    refactor: use using

--- src/main/scala/Tree.scala
+++ src/main/scala/Tree.scala
@@ -20,12 +20,12 @@ enum Tree[+T: Ordering]:
       case Node(l, y, r) => if sord.gt(x, y) then l.member(x) else go(y, r)
   }
 
-  def insert[S >: T : Ordering](x: S): Tree[S] = this match
+  def insert[S >: T](x: S)(using sord: Ordering[S]): Tree[S] = this match
     case Leaf => Node(Leaf, x, Leaf)
     case Node(l, y, r) =>
-      if summon[Ordering[S]].gt(x, y) then
+      if sord.gt(x, y) then
         Node(l, y, r.insert(x))
-      else if summon[Ordering[S]].lt(x, y) then
+      else if sord.lt(x, y) then
         Node(l.insert(x), y, r)
       else
         this
