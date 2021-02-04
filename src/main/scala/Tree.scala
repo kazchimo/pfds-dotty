6efe93ec2fbce86655f8efe888e684541fb9cfb1
kazuki <pharcydetip@gmail.com>
2021-02-05 00:24:40 +0900

    refactor: use summon

--- src/main/scala/Tree.scala
+++ src/main/scala/Tree.scala
@@ -5,12 +5,12 @@ enum Tree[+T](implicit ord: Ordering[T]):
   case Leaf 
   case Node(left: Tree[T], elem: T,  right: Tree[T])(implicit ord: Ordering[T]) 
   
-  def insert[S >: T](x: S)(implicit sord: Ordering[S]): Tree[S] = this match
+  def insert[S >: T: Ordering](x: S): Tree[S] = this match
     case Leaf => Node(Leaf, x, Leaf)
     case Node(l, y, r) => 
-        if sord.gt(x, y) then 
+        if summon[Ordering[S]].gt(x, y) then 
             Node(l, y, r.insert(x))
-        else if sord.lt(x, y) then
+        else if summon[Ordering[S]].lt(x, y) then
             Node(l.insert(x), y, r)
         else 
             this
