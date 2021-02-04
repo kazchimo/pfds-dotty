e16dc5fde1f95f73ac605ab2fcb47978b9783734
kazuki <pharcydetip@gmail.com>
2021-02-04 22:39:57 +0900

    refactor: use :: instead of Cons.apply

--- src/main/scala/LinkedList.scala
+++ src/main/scala/LinkedList.scala
@@ -18,23 +18,23 @@ enum LinkedList[+T]:
 
   def ++[S >: T](ys: LinkedList[S]): LinkedList[S] = this match
     case Nil => ys
-    case Cons(x, s) => Cons(x, s ++ ys)
+    case Cons(x, s) => x :: (s ++ ys)
   
   def ::[S >: T](y: S): LinkedList[S] = Cons(y, this)
 
   def update[S >: T](i: Int, y: S): LinkedList[S] = this match
     case Nil => throw Exception("Empty LinkedList")
-    case Cons(x, xs) if i == 0 => Cons(y, xs)
-    case Cons(x, xs) if i > 0 => Cons(x, xs.update(i - 1, y))
+    case Cons(x, xs) if i == 0 => y :: xs
+    case Cons(x, xs) if i > 0 => x :: xs.update(i - 1, y)
     case _ => throw Exception("Negative index update")
 
 
 object LinkedList:
   def empty[T]: LinkedList[T] = Nil
 
-  def cons[T](x: T, s: LinkedList[T]): LinkedList[T] = LinkedList.Cons(x, s)
+  def cons[T](x: T, s: LinkedList[T]): LinkedList[T] = x :: s
 
   def apply[T](xs: T*): LinkedList[T] = xs match
     case Seq() => Nil
-    case Seq(x, s: _*) => LinkedList.Cons(x, apply(s: _*))
+    case Seq(x, s: _*) => x :: apply(s: _*)
   
