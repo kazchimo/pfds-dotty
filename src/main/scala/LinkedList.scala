ff6f7735fc8450c0854b943933456ef4e7abe2e7
kazuki <pharcydetip@gmail.com>
2021-02-04 21:55:07 +0900

    feat: LinkedList.isEmpty

--- src/main/scala/LinkedList.scala
+++ src/main/scala/LinkedList.scala
@@ -3,6 +3,11 @@ package pfds
 enum LinkedList[+T]:
   case Nil extends LinkedList[Nothing]
   case Cons(x: T, s: LinkedList[T])
+
+  def isEmpty = this match {
+    case Nil => true
+    case _ => false
+  }
   
 object LinkedList:
   def empty[T]: LinkedList[T] = Nil
--- src/test/scala/LinkedListSpec.scala
+++ src/test/scala/LinkedListSpec.scala
@@ -1,10 +1,17 @@
 package pfds
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
+import org.scalatest.funsuite.AnyFunSuite
+import LinkedList._
 
-class LinkedListSpec extends AnyFlatSpec with Matchers:
-  it should "empty returns Nil" in {
+class LinkedListSpec extends AnyFunSuite with Matchers:
+  test("#empty") {
     LinkedList.empty[Int] shouldBe LinkedList.Nil
+  }
+
+  test("#isEmpty") {
+    Nil.isEmpty shouldBe true
+    Cons(1, Nil).isEmpty shouldBe false
   }
   
 
