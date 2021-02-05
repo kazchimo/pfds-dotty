4d9920b99a243759069f660338412f0ee993ab9c
kazuki <pharcydetip@gmail.com>
2021-02-05 17:39:01 +0900

    feat: LeftistHeap.merge

--- src/main/scala/LeftistHeap.scala
+++ src/main/scala/LeftistHeap.scala
@@ -1,9 +1,9 @@
 package pfds
 import LeftistHeap._
 
-/** 
+/**
  * Binary Tree which is heap-ordered and have leftist property.
- *  
+ *
  * heap-ordered:
  *   Any parent nodes cannot be greater than child nodes.
  *   Thus smallest value is on the root of heap.
@@ -27,11 +27,27 @@ enum LeftistHeap[+T: Ordering]:
   def rank = this match
     case Leaf => 0
     case Node(r, _, _, _) => r
+      
+  /** Merge two LeftistHeap with O(log n) order */
+  def merge[S >: T](that: LeftistHeap[S])(using sord: Ordering[S]): LeftistHeap[S] =
+    (this, that) match
+      case (Leaf, l) => l
+      case (l, Leaf) => l
+      case (Node(_, x, a1, b1), Node(_, y, a2, b2)) => 
+        if sord.lteq(x, y) then 
+          Node(x, a1, b1.merge(that)) 
+        else Node(y, a2, b2.merge(this))
 end LeftistHeap
 
 object LeftistHeap:
   /** Create a LeftistHeap containing just one value */
   def just[T: Ordering](x: T): LeftistHeap[T] = Node(1, x, Leaf, Leaf)
+  
+  def withLeft[T: Ordering](left: LeftistHeap[T], x: T): LeftistHeap[T] =
+    Node(x, left, Leaf)
+  
+  def triangle[T: Ordering](a: T, b: T, c: T): LeftistHeap[T] = 
+    Node(List(a, b, c).min, just(b), just(c))
 
   object Node:
     /** Create a Node calculating the rank and sorting child Nodes */
--- src/test/scala/LeftistHeapSpec.scala
+++ src/test/scala/LeftistHeapSpec.scala
@@ -5,6 +5,18 @@ import org.scalatest.funsuite.AnyFunSuite
 import LeftistHeap._
 
 class LeftistHeapSpec extends AnyFunSuite with Matchers:
+  test("#merge") {
+    LeftistHeap.just(1).merge(LeftistHeap.triangle(2, 3, 4)) shouldBe 
+      Node(1, 1, LeftistHeap.triangle(2, 3, 4), Leaf)
+    LeftistHeap.triangle(2, 3, 4).merge(LeftistHeap.just(1)) shouldBe
+      Node(1, 1, LeftistHeap.triangle(2, 3, 4), Leaf)
+    
+    Node(1, triangle(2, 3, 4), just(5)).merge(triangle(6, 7, 8)) shouldBe
+      Node(2, 1, triangle(2, 3, 4), withLeft(triangle(6, 7, 8), 5))
+    triangle(6, 7, 8).merge(Node(1, triangle(2, 3, 4), just(5))) shouldBe
+      Node(2, 1, triangle(2, 3, 4), withLeft(triangle(6, 7, 8), 5))
+      
+  }
 end LeftistHeapSpec
 
 class LeftistHeapCompanionSpec extends AnyFunSuite with Matchers:
