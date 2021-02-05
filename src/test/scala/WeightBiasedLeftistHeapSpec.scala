5a372e7966521ccd99f2f32a55ddd9ec71f2aff4
kazuki <pharcydetip@gmail.com>
2021-02-06 00:04:43 +0900

    test: WeightBiasedLeftistHeap.merge

--- src/test/scala/WeightBiasedLeftistHeapSpec.scala
+++ src/test/scala/WeightBiasedLeftistHeapSpec.scala
@@ -12,16 +12,26 @@ class WeightBiasedLeftistHeapSpec extends AnyFunSuite with Matchers:
     Node(0, triangle(1, 2, 3), triangle(4, 5, 6)).weight shouldBe 7
   }
   
+  test("#merge") {
+    just(1).merge(triangle(2, 3, 4)) shouldBe Node(4, 1, triangle(2, 3, 4), Leaf)
+    triangle(2, 3, 4).merge(just(1)) shouldBe Node(4, 1, triangle(2, 3, 4), Leaf)
+    
+    Node(1, triangle(2, 3, 4), just(5)).merge(triangle(6, 7, 8)) shouldBe
+      Node(8, 1, Node(4, 5, triangle(6, 7, 8), Leaf), triangle(2, 3, 4))
+    triangle(6, 7, 8).merge(Node(1, triangle(2, 3, 4), just(5))) shouldBe
+      Node(8, 1, Node(4, 5, triangle(6, 7, 8), Leaf), triangle(2, 3, 4))
+  }
+
   test("#isEmpty") {
     Leaf.isEmpty shouldBe true
     just(1).isEmpty shouldBe false
   }
-  
+
   test("#min") {
     the[Exception] thrownBy Leaf.min
     triangle(3, 2, 1).min shouldBe 1
   }
-  
+
   test("#deleteMin") {
     the[Exception] thrownBy Leaf.deleteMin
     triangle(2, 3, 1).deleteMin shouldBe Node(2, 2, Node(1, 3, Leaf, Leaf), Leaf)
@@ -32,7 +42,7 @@ class WeightBiasedLeftistHeapCompanionSpec extends AnyFunSuite with Matchers:
   test("#just") {
     just(1) shouldBe Node(1, 1, Leaf, Leaf)
   }
-  
+
   test("#triangle") {
     triangle(1, 2, 3) shouldBe Node(3, 1, just(2), just(3))
   }
