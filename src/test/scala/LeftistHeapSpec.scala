e957003ae09d18866c6a3b663e3b86ff6724537f
kazuki <pharcydetip@gmail.com>
2021-02-05 16:19:04 +0900

    test: LeftistHeap.just

--- src/test/scala/LeftistHeapSpec.scala
+++ src/test/scala/LeftistHeapSpec.scala
@@ -7,6 +7,12 @@ import LeftistHeap._
 class LeftistHeapSpec extends AnyFunSuite with Matchers:
 end LeftistHeapSpec
 
+class LeftistHeapCompanionSpec extends AnyFunSuite with Matchers:
+  test("#just") {
+    LeftistHeap.just(1) shouldBe Node(1, 1, Leaf, Leaf)
+  }
+end LeftistHeapCompanionSpec
+
 class NodeCompanionSpec extends AnyFunSuite with Matchers:
   test("#apply") {
     Node(1, Node(2, Leaf, Leaf), Leaf) shouldBe Node(1, 1, Node(1, 2, Leaf, Leaf), Leaf)
