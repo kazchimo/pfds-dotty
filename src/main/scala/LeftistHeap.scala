fba91f739405af25318140c1e870afadc101d412
kazuki <pharcydetip@gmail.com>
2021-02-05 16:22:21 +0900

    feat: add Ordering constraint to LeftistHeap.T

--- src/main/scala/LeftistHeap.scala
+++ src/main/scala/LeftistHeap.scala
@@ -2,12 +2,12 @@ package pfds
 import LeftistHeap._
 
 /** Binary Tree which has keep-ordered and leftist property */
-enum LeftistHeap[+T]:
+enum LeftistHeap[+T: Ordering]:
   case Leaf
-  case Node(r: Int, elem: T, left: LeftistHeap[T], right: LeftistHeap[T])
-  
-  /** 
-   * Rank of LeftistHeap which represents a length of right spine 
+  case Node(r: Int, elem: T, left: LeftistHeap[T], right: LeftistHeap[T])(implicit ord: Ordering[T])
+
+  /**
+   * Rank of LeftistHeap which represents a length of right spine
    * Figure:
    *       .
    *      / \    .rank
@@ -22,11 +22,11 @@ end LeftistHeap
 
 object LeftistHeap:
   /** Create a LeftistHeap containing just one value */
-  def just[T](x: T): LeftistHeap[T] = Node(1, x, Leaf, Leaf)
-  
+  def just[T: Ordering](x: T): LeftistHeap[T] = Node(1, x, Leaf, Leaf)
+
   object Node:
     /** Create a Node calculating the rank and sorting child Nodes */
-    def apply[T](x: T, a: LeftistHeap[T], b: LeftistHeap[T]): Node[T] =
+    def apply[T: Ordering](x: T, a: LeftistHeap[T], b: LeftistHeap[T]): Node[T] =
       if a.rank >= b.rank then Node(b.rank + 1, x, a, b) else Node(a.rank + 1, x, b, a)
 end LeftistHeap
 
