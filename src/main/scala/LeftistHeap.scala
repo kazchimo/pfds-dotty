23eebd78996ff42133f66c640a1ec86e8a283d42
kazuki <pharcydetip@gmail.com>
2021-02-05 15:33:32 +0900

    doc: LeftistHeap

--- src/main/scala/LeftistHeap.scala
+++ src/main/scala/LeftistHeap.scala
@@ -1,6 +1,4 @@
+/** Binary Tree which has keep-ordered and leftist property */
 enum LeftistHeap[+T]:
   case Leaf
   case Node(rank: Int, elem: T, left: LeftistHeap[T], right: LeftistHeap[T])
-  
-
-
