a3f19f426394d835cd1c0a73ab28e1675623b22d
kazuki <pharcydetip@gmail.com>
2021-02-06 20:04:58 +0900

    doc: Heap

--- src/main/scala/heap/Heap.scala
+++ src/main/scala/heap/Heap.scala
@@ -3,14 +3,18 @@ package heap
 /** A data structure which provide an efficient access to a minimum value */
 trait Heap[+T: Ordering]:
   type This[+T] <: Heap[T]
-  
+
   def isEmpty: Boolean
 
+  /** Add a new element to this heap */
   def insert[S >: T: Ordering](x: S): This[S]
 
+  /** Merge two heaps */
   def merge[S >: T: Ordering](that: This[S]): This[S]
 
+  /** The minimum value */
   def min: T
 
+  /** Delete the minimum value */
   def deleteMin: This[T] 
   
