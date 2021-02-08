cfc072c8906bff93bed9653d35208bcdd0c373ad
kazuki <pharcydetip@gmail.com>
2021-02-08 21:23:15 +0900

    refactor: add type alias

--- src/main/scala/set/RedBlackSet.scala
+++ src/main/scala/set/RedBlackSet.scala
@@ -5,8 +5,11 @@ import Color._
 enum Color:
   case Red, Black
 
+type BC = Black.type
+type RC = Red.type
+
 enum RedBlackSet[C <: Color, CL <: Color, +T, CR <: Color] extends Set[T]:
-  case Empty extends RedBlackSet[Black.type, Nothing, Nothing, Nothing]
+  case Empty extends RedBlackSet[BC, Nothing, Nothing, Nothing]
   case Node(color: C, left: RedBlackSet[CL, ?, T, ?], elem: T, right: RedBlackSet[CR, ?, T, ?])
 
   override type This[T] = RedBlackSet[C, CL, T, CR]
@@ -38,19 +41,19 @@ object RedBlackSet:
       Node(Red, Node(Black, a, x, b), y, Node(Black, c, z, d))
     case (c, a, x, b) => Node(c, a, x, b)
 
-  def just[C <: Color, T](c: C, x: T): RedBlackSet[C, Black.type, T, Black.type] = Node(c, Empty, x, Empty)
+  def just[C <: Color, T](c: C, x: T): RedBlackSet[C, BC, T, BC] = Node(c, Empty, x, Empty)
 
-  def justBlack[T](x: T): RedBlackSet[Black.type, Black.type, T, Black.type] = just(Black, x)
+  def justBlack[T](x: T): RedBlackSet[BC, BC, T, BC] = just(Black, x)
 
-  def justRed[T](x: T): RedBlackSet[Red.type, Black.type, T, Black.type] = just(Red, x)
+  def justRed[T](x: T): RedBlackSet[RC, BC, T, BC] = just(Red, x)
 
   def black[T, C1 <: Color, C2 <: Color](
       left: RedBlackSet[C1, ?, T, ?], x: T, right: RedBlackSet[C2, ?, T, ?]
-  ): RedBlackSet[Black.type, C1, T, C2] = Node(Black, left, x, right)
+  ): RedBlackSet[BC, C1, T, C2] = Node(Black, left, x, right)
 
   def red[T, C1 <: Color, C2 <: Color](
       left: RedBlackSet[C1, ?, T, ?], x: T, right: RedBlackSet[C2, ?, T, ?]
-  ): RedBlackSet[Red.type, C1, T, C2] = Node(Red, left, x, right)
+  ): RedBlackSet[RC, C1, T, C2] = Node(Red, left, x, right)
 
   def withLeft[C <: Color, CL <: Color, T](
       c: C, left: RedBlackSet[CL, ?, T, ?], x: T): RedBlackSet[C, CL, T, ?] =
