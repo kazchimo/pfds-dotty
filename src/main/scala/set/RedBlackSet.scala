136e21dff975d1628799cbfe55a9551e9e8815c1
kazuki <pharcydetip@gmail.com>
2021-02-08 22:24:19 +0900

    refactor: remove color type parameter

--- src/main/scala/set/RedBlackSet.scala
+++ src/main/scala/set/RedBlackSet.scala
@@ -7,16 +7,16 @@ enum Color:
   case Red, Black
 
 
-enum RedBlackSet[C <: Color, +T] extends Set[T]:
-  case Empty extends RedBlackSet[Black.type, Nothing]
-  case Node(color: C, left: RedBlackSet[?, T], elem: T, right: RedBlackSet[?, T])
+enum RedBlackSet[+T] extends Set[T]:
+  case Empty extends RedBlackSet[Nothing]
+  case Node(color: Color, left: RedBlackSet[T], elem: T, right: RedBlackSet[T])
 
-  override type This[T] = RedBlackSet[Black.type, T]
+  override type This[T] = RedBlackSet[T]
 
   override type Constraint[T] = Ordering[T]
 
-  override def insert[S >: T: Ordering](x: S): RedBlackSet[Black.type, S] = {
-    def ins(s: RedBlackSet[?, S]): RedBlackSet[?, S] = s match
+  override def insert[S >: T: Ordering](x: S): RedBlackSet[S] = {
+    def ins(s: RedBlackSet[S]): RedBlackSet[S] = s match
       case Empty => justRed(x)
       case Node(color, a, y, b) => 
         if Ordering[S].lt(x, y) then balance(color, ins(a), y, b)
@@ -37,9 +37,9 @@ enum RedBlackSet[C <: Color, +T] extends Set[T]:
 end RedBlackSet
 
 object RedBlackSet:
-  def balance[T, C1 <: Color, C2 <: Color, C3 <: Color](
-     color: C1, n1: RedBlackSet[C2, T], elem: T, n2: RedBlackSet[C3, T]
-  ): RedBlackSet[?, T] = (color, n1, elem, n2) match
+  def balance[T](
+     color: Color, n1: RedBlackSet[T], elem: T, n2: RedBlackSet[T]
+  ): RedBlackSet[T] = (color, n1, elem, n2) match
     case (Black, Node(Red, Node(Red, a, x, b), y, c), z, d) =>
       Node(Red, Node(Black, a, x, b), y, Node(Black, c, z, d))
     case (Black, Node(Red, a, x, Node(Red, b, y, c)), z, d) =>
@@ -50,21 +50,21 @@ object RedBlackSet:
       Node(Red, Node(Black, a, x, b), y, Node(Black, c, z, d))
     case (c, a, x, b) => Node(c, a, x, b)
 
-  def just[C <: Color, T](c: C, x: T): RedBlackSet[C, T] = Node(c, Empty, x, Empty)
+  def just[T](c: Color, x: T): RedBlackSet[T] = Node(c, Empty, x, Empty)
 
-  def justBlack[T](x: T): RedBlackSet[Black.type, T] = just(Black, x)
+  def justBlack[T](x: T): RedBlackSet[T] = just(Black, x)
 
-  def justRed[T](x: T): RedBlackSet[Red.type, T] = just(Red, x)
+  def justRed[T](x: T): RedBlackSet[T] = just(Red, x)
 
-  def black[T](left: RedBlackSet[?, T], x: T, right: RedBlackSet[?, T]): RedBlackSet[Black.type, T] =
+  def black[T](left: RedBlackSet[T], x: T, right: RedBlackSet[T]): RedBlackSet[T] =
     Node(Black, left, x, right)
 
-  def red[T](left: RedBlackSet[?, T], x: T, right: RedBlackSet[?, T]): RedBlackSet[Red.type, T] =
+  def red[T](left: RedBlackSet[T], x: T, right: RedBlackSet[T]): RedBlackSet[T] =
     Node(Red, left, x, right)
 
-  def withLeft[C <: Color, T](c: C, left: RedBlackSet[?, T], x: T): RedBlackSet[C, T] =
+  def withLeft[T](c: Color, left: RedBlackSet[T], x: T): RedBlackSet[T] =
     Node(c, left, x, Empty)
 
-  def withRight[C <: Color, T](c: C, x: T, right: RedBlackSet[?, T]): RedBlackSet[C, T] =
+  def withRight[T](c: Color, x: T, right: RedBlackSet[T]): RedBlackSet[T] =
     Node(c, Empty, x, right)
 end RedBlackSet
