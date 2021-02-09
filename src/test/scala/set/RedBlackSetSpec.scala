3b58818a347e85fe4481a19766b1bacfc4670592
kazuki <pharcydetip@gmail.com>
2021-02-09 13:45:27 +0900

    test: RedBlackSet.insert

--- src/test/scala/set/RedBlackSetSpec.scala
+++ src/test/scala/set/RedBlackSetSpec.scala
@@ -21,3 +21,17 @@ class RedBlackSetSpec extends AnyFunSuite with Matchers:
       black(justRed(1), 2, justRed(3))
   }
 
+  test("#insert") {
+    black(justRed(1), 3, justRed(5)).insert(2) shouldBe 
+      black(justBlack(1), 2, withRight(Black, 3, justRed(5)))
+    black(justRed(1), 3, justRed(5)).insert(4) shouldBe 
+      black(withLeft(Black, justRed(1), 3), 4, justBlack(5))
+    
+    red(black(justRed(1), 3, justRed(5)), 7, black(justRed(9), 11, justRed(13))).insert(4) shouldBe
+      black(
+        red(withLeft(Black, justRed(1), 3), 4, justBlack(5)),
+        7,
+        black(justRed(9), 11, justRed(13))
+      )
+  }
+
