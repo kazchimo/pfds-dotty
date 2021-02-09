213348b2886eeaa0be12c59dd25445eb3df55384
kazuki <pharcydetip@gmail.com>
2021-02-10 00:39:55 +0900

    feat: add Stream trait

---
+++ src/main/scala/stream/Stream.scala
@@ -0,0 +1,11 @@
+package stream
+
+trait Stream[+T]: 
+  def ++[S >: T](that: Stream[S]): Stream[S]
+  
+  def take(n: Int): Stream[T]
+
+  def drop(n: Int): Stream[T]
+
+  def reverse: Stream[T]
+  
