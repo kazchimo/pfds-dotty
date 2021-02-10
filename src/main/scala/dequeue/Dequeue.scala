543aba32c59447b1fd2f29d3c68441724156328b
kazuki <pharcydetip@gmail.com>
2021-02-10 16:03:54 +0900

    feat: add Dequeue trait

---
+++ src/main/scala/dequeue/Dequeue.scala
@@ -0,0 +1,14 @@
+package dequeue
+
+trait Dequeue[+T, This[+T] <: Dequeue[T, ?]]:
+  def +:[S >: T](a: S): This[S]
+
+  def head: T
+
+  def tail: This[T]
+
+  def :+[S >: T](a: S): This[S]
+
+  def last: T
+
+  def init: This[T]
